c9c79de516add2f0b598665275642946924f7c3a
Marko-NeurobotX <marko.scepanovic@neurobotx.me>
2024-06-14 12:30:07 +0200

    e

--- database/migrations/2024_06_14_095741_remove_description_column.php
+++ database/migrations/2024_06_14_095741_remove_description_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('pripremas', function($table) {
-            $table->dropIfExists('description');
+            $table->dropColumn('description');
         });
     }
 
